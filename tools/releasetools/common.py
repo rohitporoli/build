511bb720ab25836d09a4785c36d0513659ed3a04
LorDClockaN <davor@losinj.com>
2015-10-18 18:13:22 +0200

    Add SuperSU
    Thanks to chinfire's "How to SU" guide,
    I've managed to get SuperSU zip flashing on every
    rom flash and it can easily be updated in
    vendor/aosp
    
    Also Settings part merged in from OmniRom
    
    Change-Id: I0f57f8c2b8733021c3c0b9a5fd3c343ae565a959
    Signed-off-by: LorDClockaN <davor@losinj.com>
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
    String cleanup and remove Unmount system after SuperSu flash
    
    Change-Id: I35519779f26f58237bbc7f3a21a014c15d81b6bd
    Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>
    
    Conflicts:
    	tools/releasetools/common.py
    
    fixup string
    
    Conflicts:
    	tools/releasetools/common.py

--- tools/releasetools/common.py
+++ tools/releasetools/common.py
@@ -1624,8 +1624,9 @@ class BlockDifference(object):
       script.AppendExtra('if range_sha1("%s", "%s") == "%s" then' % (
                          self.device, ranges_str,
                          self._HashZeroBlocks(self.tgt.extended.size())))
-      script.Print('Verified the updated %s image.' % (partition,))
-      if partition == "system":
+      script.Print(" ")
+      script.Print("Cardinal-AOSP System files verified.")
+        if partition == "system":
         code = ErrorCode.SYSTEM_NONZERO_CONTENTS
       else:
         code = ErrorCode.VENDOR_NONZERO_CONTENTS
@@ -1635,7 +1636,8 @@ class BlockDifference(object):
           'OTA update");\n'
           'endif;' % (code, partition))
     else:
-      script.Print('Verified the updated %s image.' % (partition,))
+      script.Print(" ")
+      script.Print("Cardinal-AOSP System files verified.")
 
     if partition == "system":
       code = ErrorCode.SYSTEM_UNEXPECTED_CONTENTS
