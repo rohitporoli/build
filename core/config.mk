lineage-next/android_build_make

core: Include QCOM/MTK targets

core/config.mk
@@ -918,6 +918,12 @@ $(eval include vendor/lineage/sepolicy/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/lineage/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/lineage/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk

rohitporoli/build

Change-Id: I7e33db775e80f042ef71a43c67f32abd4b6038e7

core/config.mk
@@ -898,6 +898,12 @@ $(eval include device/common/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific apicheck.mk file
 -include $(TOPDIR)vendor/*/build/core/apicheck.mk
+
+# Rules for QCOM targets
+-include $(TOPDIR)vendor/cardinal/build/core/qcom_target.mk
+
+# Rules for MTK targets
+-include $(TOPDIR)vendor/cardinal/build/core/mtk_target.mk
 endif
 
 include $(BUILD_SYSTEM)/dumpvar.mk